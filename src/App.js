b1102b74433b338bbed8e4e25d58e4424975f414
meraj-kazi <kazimeraj11@gmail.com>
2021-05-27 18:56:29 +0600

    operation complete

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ function App() {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
     }, [todos])
 
+    function toggleTodo(id) {
+        const newTodos = [...todos]
+        const todo = newTodos.find(todo => todo.id === id)
+        todo.complete = !todo.complete
+        setTodos(newTodos)
+    }
+
     function handleAddTodo(e) {
         const name = todoNameRef.current.value
         if (name === '') return
@@ -25,13 +32,18 @@ function App() {
             })
             todoNameRef.current.value = null
     }
+
+    function handleClearTodos() {
+        const newTodos = todos.filter(todo => !todo.complete)
+        setTodos(newTodos)
+    }
   return (
       <>
-        <TodoList todos={todos} />
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
         <input ref={todoNameRef} type="text" />
         <button onClick={handleAddTodo}>Add Todo</button>
-        <button>Clear Complete</button>
-        <div>0 left Todo</div>
+        <button onClick={handleClearTodos}>Clear Complete</button>
+        <div>{todos.filter(todo => !todo.complete).length} left Todo</div>
       </>
   )
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Todo from './Todo'
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, toggleTodo }) {
     return (
         todos.map(todo => {
-            return <Todo key={todo.id} todo={todo} />
+            return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />
         })
     )
 }
